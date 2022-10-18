f34c8f5b650d3dc4ac1c9239000d5d89d76b1e4b
mehedi hasan <mehedicsebu@gmail.com>
2022-10-18 19:30:08 +0600

    print backward \ and %

---
+++ intro_prog_lang_prob_solv_part_1/prac1_prob_1_print_percent.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hi there/\\.\t I hope you are doing great and practicing well.\nNow this is a simple task you need to do.\tJust care for the identation.\nShow 100%% of sincerity");
+    return 0;
+}
