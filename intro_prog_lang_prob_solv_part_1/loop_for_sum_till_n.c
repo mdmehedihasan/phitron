5969a1c8a69d3b86fc479b6caca3e2ed9cd5b2c4
mehedi hasan <mehedicsebu@gmail.com>
2022-10-23 11:49:41 +0600

    print loop

---
+++ intro_prog_lang_prob_solv_part_1/loop_for_sum_till_n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    // scanf("%d", &n);
+    for (i = 10; i >= 1; i--)
+    {
+        printf("%d Hello\n", i);
+    }
+
+    return 0;
+}
