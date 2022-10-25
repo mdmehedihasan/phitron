4dd2dd32c2b35a13aabebfd2386fbcb5b61b1a16
mehedi hasan <mehedicsebu@gmail.com>
2022-10-26 00:16:26 +0600

    divisor of all number till N

---
+++ intro_prog_lang_prob_solv_part_1/all_divisors_for_n_numbers.c
@@ -0,0 +1,17 @@
+/*
+Write a C program to take one positive integer N as input and print all divisors of N.
+*/
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
