345f79e10d5683edc4269b9a5e055b2edde3e0b9
mehedi hasan <mehedicsebu@gmail.com>
2022-10-25 06:05:44 +0600

    chocolate calculation

--- intro_prog_lang_prob_solv_part_1/difference_is_positive.c
+++ intro_prog_lang_prob_solv_part_1/difference_is_positive.c
@@ -1,8 +1,3 @@
-/*
-Rahim was decorating the book shelf with his books. He has two bookshelves, and he wants to put equal books in each book shelf. At first he put some random number of books on both shelves by guessing numbers. Now he wants to know the difference between the number of books in these two shelves so that he can rearrange them. Help him!
-
-You will be given two integer numbers A and B , you need to print the difference between these two numbers. Remember, difference is always positive.
-*/
 #include <stdio.h>
 int main()
 {
