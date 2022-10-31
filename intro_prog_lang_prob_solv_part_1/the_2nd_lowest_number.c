060f46b014337c9ac31ba5de1e2f7c2daf84a9e5
mehedi hasan <mehedicsebu@gmail.com>
2022-10-31 23:26:16 +0600

    2nd lowest number

--- intro_prog_lang_prob_solv_part_1/the_2nd_lowest_number.c
+++ intro_prog_lang_prob_solv_part_1/the_2nd_lowest_number.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int arr[] = {6, 7, 5, 12, 11}, m1, m2, i;
+    int arr[] = {6, 7, 5, 4, 12, 2, 11, 1}, m1, m2, i;
     m1 = arr[0];
     m2 = arr[1];
 
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 8; i++)
     {
+
+        if (arr[i] < m1)
+        {
+            m2 = m1;
+            m1 = arr[i];
+        }
+
+        // if (arr[i] < m2)
+        // {
+        //     m2 = arr[i];
+        // }
     }
+    printf("%d\n", m1);
+    printf("%d", m2);
 
     return 0;
 }
