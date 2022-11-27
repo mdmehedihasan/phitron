65acbebc609af1af8d3e8671b797513e82b90443
mehedi hasan <mehedicsebu@gmail.com>
2022-11-27 20:54:54 +0600

    factorrial with function

--- intro_prog_lang_prob_solv_part_1/factorial_with_function.c
+++ intro_prog_lang_prob_solv_part_1/factorial_with_function.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+int fact(int a)
+{
+    int tot = 1, i;
+    for (i = 1; i <= a; i++)
+    {
+        tot *= i;
+    }
+    return tot;
+}
 int main()
 {
+    int a, res;
+    scanf("%d", &a);
+    res = fact(a);
+    printf("%d", res);
 
     return 0;
 }
