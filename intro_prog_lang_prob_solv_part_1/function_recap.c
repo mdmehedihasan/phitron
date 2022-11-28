7b9a09d52817a821ab5d3515dc797bf0be7c7b67
mehedi hasan <mehedicsebu@gmail.com>
2022-11-28 21:42:54 +0600

    sin function in c

---
+++ intro_prog_lang_prob_solv_part_1/function_recap.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    printf("%d\n", abs(-2));
+    return 0;
+}
---
+++ intro_prog_lang_prob_solv_part_1/math_fun.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    printf("%f", sin(3.141593 / 2));
+    return 0;
+}
