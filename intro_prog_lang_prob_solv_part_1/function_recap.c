231d1237b240e5bf5a7ac7509b8247681d87e7d8
mehedi hasan <mehedicsebu@gmail.com>
2022-11-29 15:29:39 +0600

    recursion with factorial

--- intro_prog_lang_prob_solv_part_1/function_recap.c
+++ intro_prog_lang_prob_solv_part_1/function_recap.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+int factorial(int n)
+{
+    if (n > 0)
+    {
+        return n * factorial(n - 1);
+    }
+    else
+        return 1;
+}
 int main()
 {
-    printf("%d\n", abs(-2));
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", factorial(n));
     return 0;
 }
