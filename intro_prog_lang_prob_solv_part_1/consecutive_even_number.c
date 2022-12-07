ed0d0b7ac41e6f296b78a824c45ff677b6aee092
mehedi hasan <mehedicsebu@gmail.com>
2022-12-07 20:04:27 +0600

    consecutive even numbers

--- intro_prog_lang_prob_solv_part_1/consecutive_even_number.c
+++ intro_prog_lang_prob_solv_part_1/consecutive_even_number.c
@@ -2,28 +2,27 @@
 int make_add()
 {
     int i, x, res, n, t, j, count;
-    scanf("%d", &t);
-    for (i = 0; i < t; i++)
+    scanf("%d", &n);
+    count = 0;
+    j = 0;
+    while (count != 4)
     {
-        scanf("%d", &n);
-        count = 0;
-        {
-            j = 0;
-            while (count != 4)
-            {
-                x = (n - 12) / 4;
-                res = x + j;
-                count++;
-                j += 2;
-                printf("%d ", res);
-            }
-            printf("\n");
-        }
+        x = (n - 12) / 4;
+        res = x + j;
+        count++;
+        j += 2;
+        printf("%d ", res);
     }
+    printf("\n");
 }
 int main()
 {
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        make_add();
+    }
 
-    make_add();
     return 0;
 }
