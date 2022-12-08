bb64c4c34ebade97bd11107649e27359571a27bc
mehedi hasan <mehedicsebu@gmail.com>
2022-12-08 09:41:29 +0600

    malloc and realloc

---
+++ intro_prog_lang_prob_solv_part_1/malloc_ralloc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, i;
+    n = 5;
+    int *ptr;
+    ptr = (int *)malloc(n * sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+    }
+    else
+    {
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", (ptr + i));
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", *(ptr + i));
+        }
+    }
+    n = 10;
+    ptr = realloc(ptr, n * sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+    }
+    else
+    {
+        for (i = 5; i < n; i++)
+        {
+            scanf("%d", (ptr + i));
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", *(ptr + i));
+        }
+    }
+
+    free(ptr);
+
+    return 0;
+}
